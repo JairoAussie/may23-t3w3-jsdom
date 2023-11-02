231a3f20816cda2235ac68bfa4c3f10d243b9e22
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2023-11-02 20:01:46 +1000

    pseudo form and the focus events

--- script.js
+++ script.js
@@ -40,22 +40,57 @@ function createListOfMedia(){
 function removeItemFromList(targetItem){
     let targetItemNode = document.getElementById(targetItem);
     
-    targetItemNode.parentNode.removeChild(targetItemNode);
+    //targetItemNode.parentNode.removeChild(targetItemNode);
+    favoriteMedia = favoriteMedia.filter(item => item !== targetItem);
+    //Regenerate the visuals
+    createListOfMedia();
 }
 
-function addItemToList(event){
+function addItemToList(event, targetId){
     event.preventDefault();
     //console.log(event);
     console.log("we are trying to add an item to the list");
-    let realInputField = document.getElementById("real-nameinput");
+    let inputField = document.getElementById(targetId);
     //console.log(realInputField.value);
-    let newItemName = realInputField.value;
-    favoriteMedia.push(newItemName);
-    
-    createListOfMedia();
+    let newItemName = inputField.value;
+    if (newItemName){
+        favoriteMedia.push(newItemName);
+        createListOfMedia();
+    } else {
+        console.warn("Attempted to add an item to the list");
+        console.error("Attempted to add an item to the list");
+        alert("Please introduce a value");
+    }
+   
 
 }
 
 let realFormSubmitButton = document.getElementById("real-formsubmit");
-console.log(realFormSubmitButton);
-realFormSubmitButton.addEventListener("click", addItemToList);
+// console.log(realFormSubmitButton);
+// realFormSubmitButton.addEventListener("click", (event)=> {addItemToList(event, "real-nameinput")});
+
+// helper text id: real-texthint
+function inputHelperOnFocus(targetId){
+    let helperElement = document.getElementById(targetId);
+    console.log("showing text hint now");
+    helperElement.style.display = "inherit";
+}
+
+function inputHelperOnBlur(targetId){
+    let helperElement = document.getElementById(targetId);
+    console.log("hiding text hint now");
+    helperElement.style.display = "none";
+}
+
+let realFormTextInput = document.getElementById("real-nameinput");
+realFormTextInput.addEventListener("focusin", () => {inputHelperOnFocus("real-texthint")});
+realFormTextInput.addEventListener("focusout",() => {inputHelperOnBlur("real-texthint")});
+inputHelperOnBlur("real-texthint");
+
+let pseudoFormTextInput = document.getElementById("pseudo-nameinput");
+pseudoFormTextInput.addEventListener("focusin", () => {inputHelperOnFocus("pseudo-texthint")});
+pseudoFormTextInput.addEventListener("focusout",() => {inputHelperOnBlur("pseudo-texthint")});
+inputHelperOnBlur("pseudo-texthint");
+
+let pseudoFormSubmitButton = document.getElementById("pseudo-formsubmit");
+pseudoFormSubmitButton.addEventListener("click", (event)=> {addItemToList(event, "pseudo-nameinput")});
